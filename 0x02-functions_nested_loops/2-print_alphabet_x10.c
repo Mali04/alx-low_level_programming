f4c4df745da8c118868d96609df19ed88f92f28e
Mali04 <berdad.ali@gmail.com>
2022-10-18 05:42:34 -0700

    x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Check the code
+ *
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+int y = 0;
+
+for (y ; y < 10 : y++)
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
