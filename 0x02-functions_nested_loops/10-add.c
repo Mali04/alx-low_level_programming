634d0e4e3d0f63c8c11e94f07c8ca7f5e99ad95b
Mali04 <berdad.ali@gmail.com>
2022-10-18 09:54:55 -0700

    x

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * description : add to int
+ * @a : input number
+ * @b : input number
+ * Return: Result to add a and b
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
