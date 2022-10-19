07ebe0b59cb9dba7dfb75a90c672c443c756f6c1
Mali04 <berdad.ali@gmail.com>
2022-10-19 09:45:41 -0700

    x

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * largest_number - returns the largest of 3 numbers
 * @a: first integer
@@ -18,10 +20,10 @@ else if (b >= a && b >= c)
 	{
 	largest = b;
 	}
-else
-	{
-	largest = c;
-	}
-
-return (largest);
+	else
+		{
+		largest = c;
+		}
+	
+	return (largest);
 }
