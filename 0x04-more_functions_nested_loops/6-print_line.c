18a5749ae7d888e0a45b8b6a850c423e96ba2ba6
Mali04 <berdad.ali@gmail.com>
2022-10-20 04:45:41 -0700

    x

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - check code
+ * @n: input int
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+	{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i <= n; i++)
+		_putchar('_');
+	}
+	_putchar('\n');
+	}
+
