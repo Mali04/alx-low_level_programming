23a708f803407f928035f1e9bd6f1937abe6e9a2
Mali04 <berdad.ali@gmail.com>
2022-10-24 05:45:07 -0700

    x

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,10 @@ void print_rev(char *s)
 	int len = 0;
 	int i;
 
-	while (str[len] != '\0')
+	while (s[len] != '\0')
 		len++;
 	for (i = len - 1; i >= 0; i--)
-		_putchar(str[i]);
+		_putchar(s[i]);
 	_putchar('\n');
 }
 
