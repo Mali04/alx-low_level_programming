095e663de6c500394f782fccdf73bdb1bb2eefaa
Mali04 <berdad.ali@gmail.com>
2022-10-24 05:15:22 -0700

    x

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int -  check code.
+ * @a: an inout integer pointer
+ * @b: an input integer pointer
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	aux = *a;
+	*a = *b;
+	*b = c;
+}
