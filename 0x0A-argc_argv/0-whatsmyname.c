4ab989f6eba104924277a9d50ee80281c60649a0
Mali04 <berdad.ali@gmail.com>
2022-11-07 03:16:50 -0800

    x

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check code
+ * @argc: count of arg
+ * @argv: array of arg
+ * Return: 0.
+ */
+
+int main(int argc, char* argv[])
+{
+printf("%s\n",argv[0]);
+return(0);
+}
+
